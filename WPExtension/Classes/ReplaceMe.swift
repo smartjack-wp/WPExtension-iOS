c7328d344f7b29d66eee2eec0f4e0caabe321b35
onemoon <sopthfg@gmail.com>
2021-01-18 15:10:36 +0900

    commit with tag

---
+++ WPExtension/Classes/ReplaceMe.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+open class TestClass {
+    func helloLibraryInternal() {
+        print("hello 0.1.1")
+    }
+    
+    open func helloLibraryOpen() {
+        print("hello 0.1.1 open")
+    }
+}
