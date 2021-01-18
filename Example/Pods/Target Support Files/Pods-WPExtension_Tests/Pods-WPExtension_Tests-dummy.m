e9fb9891c8303cfd272ca532a781fece9770d18b
onemoon <sopthfg@gmail.com>
2021-01-18 14:53:47 +0900

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-WPExtension_Tests/Pods-WPExtension_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WPExtension_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WPExtension_Tests
+@end
---
+++ Example/Pods/Target Support Files/WPExtension/WPExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WPExtension : NSObject
+@end
+@implementation PodsDummy_WPExtension
+@end
